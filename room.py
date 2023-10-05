f0333786840a0722abb79f1ff7232a14ef0e11c4
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-05 22:20:07 +0530

    Room module

---
+++ room.py
@@ -0,0 +1,8 @@
+class Room:
+    def __init__(self, name, description, north=None, south=None, east=None, west=None):
+        self.name = name
+        self.description = description
+        self.north = north
+        self.south = south
+        self.east = east
+        self.west = west
