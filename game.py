c5c5ef90ea984c5689130a1827f651fb47011f76
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-05 22:23:34 +0530

    initialize

--- game.py
+++ game.py
@@ -1,2 +1,8 @@
 from room import Room
 
+def play_game():
+    room1 = Room("Room 1", "You are in the first room.", north=None, south=None, east="room2", west=None)
+    room2 = Room("Room 2", "You are in the second room.", north=None, south=None, east=None, west="room1")
+
+    current_room = room1
+
